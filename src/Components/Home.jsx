0124f9a3c0f4f9284405753d541ee26d926fc29c
Faisal <biddafaisal@gmail.com>
2023-02-07 01:59:16 +0530

    Added animation

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/solid'
-import { homeData } from '../../data'
-import HomeItem from './HomeItem'
 import image1 from "../assets/img1.webp"
+import eco from "../assets/eco-friendly.png"
+import world from "../assets/world.png"
 import { motion } from "framer-motion"
+import { Link } from 'react-scroll'
+
 
 
 
@@ -12,12 +13,14 @@ const Home = () => {
     return (
         <>
             <div id='home' className="flex flex-col md:flex-row h-screen justify-center items-center p-10 ">
+         <img src={eco} alt="" className='w-10 absolute left-4 bottom-3'/>
+         <img src={world} alt="" className='w-10 absolute right-4 bottom-3'/>
                 <motion.div
                     initial={{ x: 0, opacity: 0 }}
                     whileInView={{ x: [-250, 0], opacity: 1 }}
                     transition={{ duration: 1 }}
 
-                    className="md:w-3/5 md:ml-10">
+                    className="md:w-3/5 md:ml-20">
                     <img src={image1} alt="" className='w-full shadow-[10px_10px_black]' />
                 </motion.div>
                 <motion.div
@@ -25,8 +28,8 @@ const Home = () => {
                     whileInView={{ x: [250, 0], opacity: 1 }}
                     transition={{ duration: 1 }}
                     className="w-2/3 flex flex-col text-center mx-20 mt-20 justify-center items-center">
-                    <h1 className='text-5xl'>Sustainable Product</h1>
-                    <button className='text-xl bg-white rounded- w-40 p-2 font-bold font-serif border-black border-2 hover:bg-red-300 hover:border-2 transition-all mt-6 shadow-[4px_4px_black]'>shop</button>
+                    <h1 className='md:text-5xl text-3xl'><i className='bg-gradient-to-r from-green-300 to-green-600 bg-clip-text text-transparent'>Sustainable products</i> at affordable prices</h1>
+                    <button className='text-xl bg-white rounded- w-40 p-2 font-bold font-serif border-black border-2 hover:bg-green-300 hover:border-2 transition-all mt-6 shadow-[4px_4px_black]'><Link to="products" smooth>shop</Link></button>
                 </motion.div>
             </div>
         </>
--- src/Components/ProductItem.jsx
+++ src/Components/ProductItem.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import { motion } from "framer-motion"
-import { PlusCircleIcon, HeartIcon } from '@heroicons/react/24/outline'
 
 
-const ProductItem = ({ src, desc, id }) => {
+
+
+const ProductItem = ({ src, desc, id}) => {
   return (
-    <div className='mx-10 shadow-2xl text-center p-2 rounded-md'>
+    <motion.div
+    initial={{ x: 0, opacity: 0 }}
+    whileInView={{ x: [250, 0], opacity: 1 }}
+    transition={{ duration: 1 }}
+
+      className='mx-10 shadow-2xl text-center p-2 rounded-md'>
       <img src={src} alt="" className='w-[32rem]' />
       <h1 className='mt-2 text-xl'>{desc}</h1>
       <h1 className='mt-2 text-sm border border-black w-28 h-6 text-black font-bold hover:bg-black hover:text-white cursor-pointer rounded-full mx-auto'>Show More</h1>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-10 hover:fill-red-500 cursor-pointer float-left">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-10 hover:fill-red-500 cursor-pointer float-left">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
       </svg>
 
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-10 float-right cursor-pointer">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-10 float-right cursor-pointer">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
       </svg>
 
-    </div>
+    </motion.div>
   )
 }
 
